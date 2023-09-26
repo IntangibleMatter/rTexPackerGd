49639883b502f044046d0e61f274922a7a17e376
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-09-25 19:02:44 -0700

    rtpb

--- addons/r_texture_packer_importer/r_tex_import.gd
+++ addons/r_texture_packer_importer/r_tex_import.gd
@@ -10,7 +10,7 @@ func _get_visible_name() -> String:
 
 
 func _get_recognized_extensions() -> PackedStringArray:
-	return ["rtpa", "json", "xml"]
+	return ["rtpa", "rptb", "json", "xml"]
 
 
 func _get_save_extension() -> String:
@@ -44,6 +44,8 @@ func _import(source_file: String, save_path: String, options: Dictionary, platfo
 	match source_file.split(".")[-1]:
 		"rtpa":
 			tex = import_rtpa(file, options)
+		"rtpb":
+			tex = import_rtpb(file, options)
 		"json":
 			tex = import_json(file, options)
 		"xml":
@@ -72,16 +74,110 @@ func import_rtpa(file: FileAccess, opt: Dictionary) -> Dictionary:
 		var spr_f := PackedInt32Array(spr.slice(2))
 		# Sprite info:   s <nameId> <originX> <originY> <positionX> <positionY> <sourceWidth> <sourceHeight> <padding> <trimmed> <trimRecX> <trimRecY> <trimRecWidth> <trimRecHeight>
 		if spr[0] == "s":
-			if spr_f[7] == 1: # trimmed
-				atlas.region = Rect2i(spr_f[8], spr_f[9], spr_f[10] + spr_f[6], spr_f[11] + spr_f[6])
+			if spr_f[8] == 1: # trimmed
+				atlas.region = Rect2i(spr_f[9], spr_f[10], spr_f[11] + spr_f[6], spr_f[12] + spr_f[6])
 			else:
 				atlas.region = Rect2i(spr_f[2], spr_f[3], spr_f[4] + spr_f[6], spr_f[5] + spr_f[6])
 			
 		
 			data[spr[1]] = AtlasTexture.new()
+		elif spr[0] == "a":
+			if spr_f[3] == 1:
+				push_error("Error: rTexPackerGd can't import font files.")
+				return data
 	
 	return data
 
+
+#  rTexPacker Binary File Structure (.rtpb)
+#  ------------------------------------------------------
+#  Offset  | Size    | Type       | Description
+#  ------------------------------------------------------
+#  File header (8 bytes)
+#  0       | 4       | char       | Signature: "rTPb"
+#  4       | 2       | short      | Version: 200
+#  6       | 2       | short      | reserved
+
+#  General info data (16 bytes)
+#  8       | 4       | int        | Sprites packed
+#  12      | 4       | int        | Flags: 0-Default, 1-Atlas image included
+#  16      | 2       | short      | Font type: 0-No font, 1-Normal, 2-SDF
+#  18      | 2       | short      | Font size
+#  20      | 2       | short      | Font SDF padding
+#  22      | 2       | short      | reserved
+
+#  Sprites properties data
+#   - Size (only sprites): 128 + 48 bytes
+#   - Size (font sprites): 128 + 64 bytes
+#  foreach (sprite.packed)
+#  {
+        #  Default sprites data (128 + 48 bytes)
+#    ...   | 128     | char       | Sprite Name identifier
+#    ...   | 4       | int        | Sprite Origin X
+#    ...   | 4       | int        | Sprite Origin Y
+#    ...   | 4       | int        | Sprite Position X
+#    ...   | 4       | int        | Sprite Position Y
+#    ...   | 4       | int        | Sprite Source Width
+#    ...   | 4       | int        | Sprite Source Height
+#    ...   | 4       | int        | Sprite Padding
+#    ...   | 4       | int        | Sprite is trimmed?
+#    ...   | 4       | int        | Sprite Trimmed Rectangle X
+#    ...   | 4       | int        | Sprite Trimmed Rectangle Y
+#    ...   | 4       | int        | Sprite Trimmed Rectangle Width
+#    ...   | 4       | int        | Sprite Trimmed Rectangle Height
+#       if (atlas.isFont)
+#       {
+        #  Additional font data (16 bytes)
+#    ...   | 4       | int        | Character unicode value
+#    ...   | 4       | int        | Character offset x
+#    ...   | 4       | int        | Character offset y
+#    ...   | 4       | int        | Character advance x
+#       }
+#  }
+func import_rtpb(file: FileAccess, opt: Dictionary) -> Dictionary:
+	var data : Dictionary
+	var bytes : PackedByteArray = file.get_buffer(file.get_length())
+	var img : Texture2D = load(opt.image)
+
+	if not bytes.slice(0, 5).get_string_from_ascii() == "rTPb":
+		return data
+	if not bytes.decode_s32(12) == 0:
+		return data
+	if not bytes.decode_s32(16) == 0:
+		push_error("Error: rTexPackerGd can't import font files.")
+		return data
+	
+	var spr_count := bytes.decode_s32(8)
+	var imgs : Array[PackedByteArray]
+
+	for spr in spr_count:
+		imgs.append(bytes.slice(24 + spr * 176, 24 + spr * 176 + 177))
+	
+	for im in imgs:
+		var atlas := AtlasTexture.new()
+		atlas.atlas = img
+
+		var name := im.slice(0, 129).get_string_from_utf8()
+		var i := 128
+		# remove the text from the start for ease of use
+		while i >= 0:
+			im.remove_at(i)
+			i -= 1
+		
+		var ints : PackedInt32Array
+
+		for j in int(im.size() / 4):
+			ints.append(im.decode_s32(j * 4))
+
+		if ints[8] == 1:
+			atlas.region = Rect2i(ints[9], ints[10], ints[11] + ints[6], ints[12] + ints[6])
+		else:
+			atlas.region = Rect2i(ints[2], ints[3], ints[4] + ints[6], ints[5] + ints[6])
+		
+		data[name] = atlas
+			
+	
+	return data
 
 func import_json(file: FileAccess, opt: Dictionary) -> Dictionary:
 	var data : Dictionary
@@ -97,6 +193,9 @@ func import_json(file: FileAccess, opt: Dictionary) -> Dictionary:
 			if not j_data.software.name.begins_with("rTexPacker"):
 				return data
 	if not j_data.has("sprites"):
+		return data
+	if j_data.atlas.is_font:
+		push_error("Error: rTexPackerGd can't import font files.")
 		return data
 	
 	for spr in j_data.sprites:
