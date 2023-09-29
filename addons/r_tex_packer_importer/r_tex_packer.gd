407468f5d3dbc673bba6376a4a2e9b9a355ded7b
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-09-29 16:56:34 -0700

    fix project settings

--- addons/r_tex_packer_importer/r_tex_packer.gd
+++ addons/r_tex_packer_importer/r_tex_packer.gd
@@ -4,24 +4,24 @@ extends EditorPlugin
 var import_plugin : EditorImportPlugin
 
 func _enter_tree() -> void:
-	var settings := get_editor_interface().get_editor_settings()
-	settings.set("r_tex_packer/import_xml", false)
-	var property_info_xml := {
-		"name": "r_tex_packer/import_xml",
-		"type": TYPE_BOOL,
-		"hint": PROPERTY_HINT_NONE,
-		"hint_string": "Enables importing XML files. Disabled by default to avoid conflicts."
-	}
-	settings.set("r_tex_packer/import_json", false)
-	var property_info_json := {
-		"name": "r_tex_packer/import_json",
-		"type": TYPE_BOOL,
-		"hint": PROPERTY_HINT_NONE,
-		"hint_string": "Enables importing JSON files. Disabled by default to avoid conflicts."
-	}
-	settings.add_property_info(property_info_xml)
-	settings.add_property_info(property_info_json)
+	if not ProjectSettings.has_setting("r_tex_packer/import_xml"):
+		ProjectSettings.set("r_tex_packer/import_xml", false)
+		var property_info_xml := {
+			"name": "r_tex_packer/import_xml",
+			"type": TYPE_BOOL,
+			"hint_string": "Enables importing XML files. Disabled by default to avoid conflicts."
+		}
+		ProjectSettings.add_property_info(property_info_xml)
+	if not ProjectSettings.has_setting("r_tex_packer/import_json"):
+		ProjectSettings.set("r_tex_packer/import_json", false)
+		var property_info_json := {
+			"name": "r_tex_packer/import_json",
+			"type": TYPE_BOOL,
+			"hint_string": "Enables importing JSON files. Disabled by default to avoid conflicts."
+		}
+		ProjectSettings.add_property_info(property_info_json)
 	
+	import_plugin = preload("res://addons/r_tex_packer_importer/r_tex_import.gd").new()
 	add_import_plugin(import_plugin)
 
 
